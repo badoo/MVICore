71fbf6600340ed905829d1461288a5319e91524b
Zbigniew Malinowski <zbigniew.malinowski@corp.badoo.com>
2018-05-02 18:27:29 +0100

    fixed scope binding

--- MVICore/src/main/java/com/badoo/mvicore/binder/internal/ScopedBinding.kt
+++ MVICore/src/main/java/com/badoo/mvicore/binder/internal/ScopedBinding.kt
@@ -2,16 +2,22 @@ package com.badoo.mvicore.binder.internal
 
 import com.badoo.mvicore.binder.Binder
 import com.badoo.mvicore.lifecycle.Lifecycle
+import com.badoo.mvicore.lifecycle.Lifecycle.Event.CREATE
 import com.badoo.mvicore.lifecycle.Lifecycle.Event.DESTROY
-import io.reactivex.Observable
+import io.reactivex.Observable.empty
+import io.reactivex.Observable.wrap
 import io.reactivex.ObservableSource
 import io.reactivex.functions.Consumer
 
 internal class ScopedBinding<T : Any>(private val scopeProvider: Lifecycle) : Binder.OneWayBinding<T, T> {
 
     override fun bind(source: ObservableSource<T>, consumer: Consumer<T>) {
-        Observable.wrap(scopeProvider).takeUntil { it == DESTROY }.flatMap { source }
-            .subscribe(consumer)
+        wrap(scopeProvider).switchMap {
+            when (it) {
+                CREATE -> source
+                DESTROY -> empty()
+            }
+        }.subscribe(consumer)
     }
 
     override fun bind(pair: Pair<ObservableSource<T>, Consumer<T>>) = bind(pair.first, pair.second)

c1613337522b198e424db622d7c75946b7751cd0
Zsolt Kocsi <zsolt.kocsi@gmail.com>
2018-05-26 14:54:43 +0100

    Simplify Binder

--- MVICore/src/main/java/com/badoo/mvicore/binder/Bindable.kt
+++
@@ -1,6 +0,0 @@
-package com.badoo.mvicore.binder
-
-import io.reactivex.ObservableSource
-import io.reactivex.functions.Consumer
-
-interface Bindable<In, Out> : ObservableSource<Out>, Consumer<In>
--- MVICore/src/main/java/com/badoo/mvicore/lifecycle/internal/FromObservableSource.kt
+++
@@ -1,6 +0,0 @@
-package com.badoo.mvicore.lifecycle.internal
-
-import com.badoo.mvicore.lifecycle.Lifecycle
-import io.reactivex.ObservableSource
-
-internal class FromObservableSource(source: ObservableSource<Lifecycle.Event>) : Lifecycle, ObservableSource<Lifecycle.Event> by source

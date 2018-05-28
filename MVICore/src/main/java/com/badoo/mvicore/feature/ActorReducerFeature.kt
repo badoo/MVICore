1c8cd4e92db49cc1f4afa0c22aaaf9cb895cd9a7
Zsolt Kocsi <zsolt.kocsi@gmail.com>
2018-05-28 10:28:09 +0100

    Add optional bootstrapper to derived features as well

--- MVICore/src/main/java/com/badoo/mvicore/feature/ActorReducerFeature.kt
+++ MVICore/src/main/java/com/badoo/mvicore/feature/ActorReducerFeature.kt
@@ -1,14 +1,17 @@
 package com.badoo.mvicore.feature
 
 import com.badoo.mvicore.element.Actor
+import com.badoo.mvicore.element.Bootstrapper
 import com.badoo.mvicore.element.Reducer
 
 open class ActorReducerFeature<Wish : Any, in Effect : Any, State : Any>(
     initialState: State,
+    bootstrapper: Bootstrapper<Wish>? = null,
     actor: Actor<State, Wish, Effect>,
     reducer: Reducer<State, Effect>
 ) : DefaultFeature<Wish, Wish, Effect, State>(
     initialState = initialState,
+    bootstrapper = bootstrapper,
     wishToAction = { wish -> wish },
     actor = actor,
     reducer = reducer

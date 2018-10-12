8b3e3dc87349154bac3ae20fb095f5cf4c4531ca
Nickolay Chameyev <nickolay.chameyev@corp.badoo.com>
2018-10-12 19:04:50 +0100

    Add more checks to travis build script

--- scripts/run-tests-travis.sh
+++ scripts/run-tests-travis.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./gradlew --settings-file settings-travis.gradle :mvicore:test --info
+./gradlew --settings-file settings-travis.gradle :mvicore:test :mvicore-android:assembleDebug :mvicore-debugdrawer:assembleDebug :mvicore-demo:mvicore-demo-app:assembleDebug --info --stacktrace

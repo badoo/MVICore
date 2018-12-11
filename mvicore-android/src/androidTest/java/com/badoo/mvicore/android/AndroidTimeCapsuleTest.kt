774de4e5a1a828ac7758dec00a288cbbf8b05076
alexanderlitovchenko <alexander.litovchenko@corp.badoo.com>
2018-12-11 11:53:58 +0000

    Add simple AndroidTimeCapsuleTest

---
+++ mvicore-android/src/androidTest/java/com/badoo/mvicore/android/AndroidTimeCapsuleTest.kt
@@ -0,0 +1,43 @@
+package com.badoo.mvicore.android
+
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import org.junit.Test
+
+class AndroidTimeCapsuleTest {
+
+    @Test
+    fun saveAndRestoreState() {
+        val timeCapsule = AndroidTimeCapsule(null)
+        timeCapsule.register("test_key") { SimpleTestParcelable("test_value") }
+
+        val bundle = Bundle()
+        timeCapsule.saveState(bundle)
+
+        val restoredTimeCapsule = AndroidTimeCapsule(bundle)
+        val restoredState = restoredTimeCapsule.get<SimpleTestParcelable>("test_key")
+
+        assert(restoredState != null)
+        assert(restoredState?.value == "test_value")
+    }
+
+    class SimpleTestParcelable(val value: String) : Parcelable {
+
+        constructor(source: Parcel) : this(source.readString())
+
+        override fun describeContents() = 0
+
+        override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+            writeString(value)
+        }
+
+        companion object {
+            @JvmField
+            val CREATOR: Parcelable.Creator<SimpleTestParcelable> = object : Parcelable.Creator<SimpleTestParcelable> {
+                override fun createFromParcel(source: Parcel): SimpleTestParcelable = SimpleTestParcelable(source)
+                override fun newArray(size: Int): Array<SimpleTestParcelable?> = arrayOfNulls(size)
+            }
+        }
+    }
+}

37a9448a2e8c1b04b7322b281081d5869867cea9
Zsolt Kocsi <zsolt.kocsi@gmail.com>
2019-02-21 16:04:47 +0000

    Add Lifecycle extension methods

---
+++ mvicore-android/src/main/java/com/badoo/mvicore/android/lifecycle/LifecycleExtensions.kt
@@ -0,0 +1,16 @@
+package com.badoo.mvicore.android.lifecycle
+
+import android.arch.lifecycle.Lifecycle
+import com.badoo.mvicore.binder.Binder
+
+fun Lifecycle.createDestroy(f: Binder.() -> Unit) {
+    Binder(CreateDestroyBinderLifecycle(this)).apply(f)
+}
+
+fun Lifecycle.startStop(f: Binder.() -> Unit) {
+    Binder(StartStopBinderLifecycle(this)).apply(f)
+}
+
+fun Lifecycle.resumePause(f: Binder.() -> Unit) {
+    Binder(ResumePauseBinderLifecycle(this)).apply(f)
+}

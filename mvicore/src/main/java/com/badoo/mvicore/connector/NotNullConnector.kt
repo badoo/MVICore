15d0cb569b48f288b32a91131740df88b582313c
Andrei Shikov <andreiishikov@gmail.com>
2019-05-04 00:17:39 +0100

    Make not null connector internal

--- mvicore/src/main/java/com/badoo/mvicore/connector/NotNullConnector.kt
+++ mvicore/src/main/java/com/badoo/mvicore/connector/NotNullConnector.kt
@@ -2,7 +2,7 @@ package com.badoo.mvicore.connector
 
 import io.reactivex.Observable
 
-class NotNullConnector<Out, In>(private val mapper: (Out) -> In?): Connector<Out, In> {
+internal class NotNullConnector<Out, In>(private val mapper: (Out) -> In?): Connector<Out, In> {
     override fun invoke(element: Out): Observable<In> =
         mapper(element)
             ?.let { Observable.just(it) }

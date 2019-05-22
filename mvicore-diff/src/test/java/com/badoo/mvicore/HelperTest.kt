469e6bdecb36261ceca15222f75bf79f6d689de5
Andrei Shikov <andreiishikov@gmail.com>
2019-05-23 00:31:52 +0100

    Add tests for watcher and helpers api

---
+++ mvicore-diff/src/test/java/com/badoo/mvicore/HelperTest.kt
@@ -0,0 +1,58 @@
+package com.badoo.mvicore
+
+import com.badoo.mvicore.util.Model
+import com.badoo.mvicore.util.testWatcher
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelperTest {
+    @Test
+    fun `by value strategy compares by value`() {
+        val results = testWatcher<List<String>>(
+            listOf(
+                Model(list = listOf("")),
+                Model(list = listOf(""))
+            )
+        ) { updates ->
+            watch(Model::list, byValue()) {
+                updates += it
+            }
+        }
+
+        assertEquals(1, results.size)
+    }
+
+    @Test
+    fun `by ref strategy compares by reference`() {
+        val results = testWatcher<List<String>>(
+            listOf(
+                Model(list = listOf("")),
+                Model(list = listOf(""))
+            )
+        ) { updates ->
+            watch(Model::list, byRef()) {
+                updates += it
+            }
+        }
+
+        assertEquals(2, results.size)
+    }
+
+    @Test
+    fun `self accesses the whole model`() {
+        val diff: DiffStrategy<Model> = { p1, p2 -> p2.list == p1?.list }
+
+        val results = testWatcher<List<String>>(
+            listOf(
+                Model(list = listOf("")),
+                Model(list = listOf(""))
+            )
+        ) { updates ->
+            watch(self(), diff) {
+                updates += it.list
+            }
+        }
+
+        assertEquals(1, results.size)
+    }
+}

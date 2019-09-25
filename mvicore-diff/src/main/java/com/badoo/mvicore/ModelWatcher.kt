7a5f83d11abd6e4dccfef57af6ed9d8eac8fbbf1
Andrei Shikov <andreiishikov@gmail.com>
2019-09-25 15:13:57 +0100

    Update model watcher to add sealed class support

--- mvicore-diff/src/main/java/com/badoo/mvicore/ModelWatcher.kt
+++ mvicore-diff/src/main/java/com/badoo/mvicore/ModelWatcher.kt
@@ -1,11 +1,19 @@
 package com.badoo.mvicore
 
-class ModelWatcher<Model> private constructor(
-    private val watchers: List<Watcher<Model, Any?>>
+class ModelWatcher<Model : Any> private constructor(
+    private val watchers: List<Watcher<Model, Any?>>,
+    private val childWatchers: HashMap<Class<Any>, ModelWatcher<Any>>
 ) {
     private var model: Model? = null
 
     operator fun invoke(newModel: Model) {
+        if (!checkChildren(newModel)) {
+            checkSelf(newModel)
+            model = newModel
+        }
+    }
+
+    private fun checkSelf(newModel: Model) {
         val oldModel = model
         watchers.forEach { element ->
             val getter = element.accessor
@@ -14,8 +22,37 @@ class ModelWatcher<Model> private constructor(
                 element.callback(new)
             }
         }
+    }
 
-        model = newModel
+    private fun checkChildren(newModel: Model): Boolean {
+        val recordedClass = childWatchers.keys.firstOrNull { it.isInstance(newModel) }
+        if (recordedClass != null) {
+            val targetWatcher = childWatchers[recordedClass]
+            targetWatcher?.invoke(newModel)
+            clearModels(targetWatcher)
+            return true
+        }
+
+        clearModels(selectedChild = null)
+        return false
+    }
+
+    private fun clearModels(selectedChild: ModelWatcher<Any>?) {
+        if (selectedChild == null) {
+            // Clear children only
+            childWatchers.values.forEach {
+                it.model = null
+            }
+        } else {
+            // Clear children and self
+            childWatchers.values.forEach {
+                if (it !== selectedChild) {
+                    it.model = null
+                }
+            }
+            model = null
+        }
+        return
     }
 
     private class Watcher<Model, Field>(
@@ -24,12 +61,14 @@ class ModelWatcher<Model> private constructor(
         val diff: DiffStrategy<Field>
     )
 
-    class Builder<Model> @PublishedApi internal constructor() {
+    class Builder<Model : Any> @PublishedApi internal constructor() : WatchDsl<Model> {
         private val watchers = mutableListOf<Watcher<Model, Any?>>()
+        @PublishedApi
+        internal val childWatchers = hashMapOf<Class<Any>, ModelWatcher<Any>>()
 
-        fun <Field> watch(
+        override fun <Field> watch(
             accessor: (Model) -> Field,
-            diff: DiffStrategy<Field> = byValue(),
+            diff: DiffStrategy<Field>,
             callback: (Field) -> Unit
         ) {
             watchers += Watcher(
@@ -41,40 +80,14 @@ class ModelWatcher<Model> private constructor(
 
         @PublishedApi
         internal fun build(): ModelWatcher<Model> =
-            ModelWatcher(watchers)
-
-        /*
-         * Syntactic sugar around watch (scoped inside the builder)
-         */
-
-        operator fun <Field> ((Model) -> Field).invoke(callback: (Field) -> Unit) {
-            watch(this, callback = callback)
-        }
-
-        infix fun <Field> ((Model) -> Field).using(pair: Pair<DiffStrategy<Field>, (Field) -> Unit>) {
-            watch(this, pair.first, pair.second)
-        }
-
-        operator fun <Field> (DiffStrategy<Field>).invoke(callback: (Field) -> Unit) =
-            this to callback
-
-        infix fun <Field1, Field2> ((Model) -> Field1).or(f: (Model) -> Field2): DiffStrategy<Model> =
-            { old, new -> this(old) != this(new) || f(old) != f(new) }
-
-        infix fun <Field1, Field2> ((Model) -> Field1).and(f: (Model) -> Field2): DiffStrategy<Model> =
-            { old, new -> this(old) != this(new) && f(old) != f(new) }
-
-        operator fun DiffStrategy<Model>.invoke(callback: (Model) -> Unit) {
-            watch(
-                accessor = { it },
-                diff = this,
-                callback = callback
-            )
-        }
+            ModelWatcher(watchers, childWatchers)
     }
 }
 
-inline fun <Model> modelWatcher(init: ModelWatcher.Builder<Model>.() -> Unit): ModelWatcher<Model> =
-    ModelWatcher.Builder<Model>()
-        .apply(init)
-        .build()
+internal interface BuilderBase<Model> {
+    fun <Field> watch(
+        accessor: (Model) -> Field,
+        diff: DiffStrategy<Field> = byValue(),
+        callback: (Field) -> Unit
+    )
+}
